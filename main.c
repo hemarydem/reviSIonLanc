183b4cb0d85de3c7e79fa7bdcfc4f9c28bad5e86
R H <m.rehamed@gmail.com>
2020-11-12 18:06:41 +0100

    jusqu'a tableau de 20 caractère

--- main.c
+++ main.c
@@ -4,11 +4,11 @@
 #include "func.h"
 //to compile gcc main.c func.c -o prg.exe don't need .exe if you are on mac
 int main(int argc, char ** argv) {
-     // un caractère
+     //------- un caractère-----------//
     char firstChar = 'a';
-    printf('%c\n', firstChar);
+    printf("%c\n", firstChar);
 
-    // un tableau de caractère
+    //------- un tableau de caractèr-----------//e
     char arrChar[3];
     arrChar[0] = 'a';
     arrChar[1] = 'b';
@@ -18,19 +18,28 @@ int main(int argc, char ** argv) {
 
     while (i < 3)
     {
-        printf("arrChar[%d] = ‰c", i,arrChar[i]);
+        printf("arrChar[%d] = %c\n", i,arrChar[i]);
         i++;
     }
     printf("\n");
     
-    //une constante de chaine de caractère
-    const char toto = "hello";
+    //------- une constante de chaine de caractère-----------//
+    const char *toto = "hello";
     char *tata = "coucou";
 
     printf("toto = %s, tata = %s\n", toto, tata);
 
-    //chaine de caractère longeur 20
+    //------- chaine de caractère longeur 20-----------//
+    char * string = NULL;
+    string = malloc(sizeof(char) * 20);
+    if(string != NULL) {
+        for(int j = 0; j < 10; j++) {
+            string[j] = 'y';
+            string[j + 1] = 'o';
+        }
+        printf("string = %s\n", string);
+    }
+    free(string);
 
-    return 0;
     return 0;
 }
