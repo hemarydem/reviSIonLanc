6f8c91c19ff817380982a8bad3ed883d0a296135
R H <m.rehamed@gmail.com>
2020-11-15 22:09:31 +0100

    collage consigne

--- main.c
+++ main.c
@@ -3,6 +3,11 @@
 #include <string.h>
 #include "func.h"
 //to compile gcc main.c func.c -o prg.exe don't need .exe if you are on mac
+/*
+# Construire un programme permettant la saisie d'une chaîne de 
+# caractères et permutant ses deux moitiés. Afficher le résultat.
+# "abcdef" donne par exemple "defabc", "abcdefg" donne par exemple "defgabc".
+*/
 int main(int argc, char ** argv) {
     return 0;
 }
