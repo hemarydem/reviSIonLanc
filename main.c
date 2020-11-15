83f0e1ad3044859a035ceeba9d610411f1dd5b13
R H <m.rehamed@gmail.com>
2020-11-15 16:48:42 +0100

    exo 4 fini

--- main.c
+++ main.c
@@ -1,8 +1,39 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include "func.h"
-//to compile gcc main.c func.c -o prg.exe don't need .exe if you are on mac
+//#include "func.h"
+
+
+/*
+2 chaines de char string et newString
+2 pointeurs ptr = newString, et ptr2 = string
+
+- tant que l'on trouve le mot à remplacer, on affecte à ptr l'adresse du mot à remplacer
+- on strcpy à ptr le nouveau mot (donc écrase dans newString)
+- on met dans ptr2 la position juste après le mot remplacé (dans String) 
+- on se place juste apres le nouveau mot (dans newString)
+- on strcpy à ptr (apres le new mot) le reste de la chaine String (strcpy(ptr, ptr2)
+
+*/
+
 int main(int argc, char ** argv) {
+    char *pose;
+    char *pose2;
+    char *string;
+    char *stringNew;
+    string = malloc(sizeof(char) * 200);
+    stringNew = malloc(sizeof(char) * 200);
+    strcpy(string, argv[1]);
+    strcpy(stringNew, argv[1]);
+    pose = string;
+    pose2 = stringNew;
+    while( (pose = strstr(pose, argv[2])) != NULL ) {
+        strcpy(pose,argv[3]);
+        pose += strlen(argv[3]);
+        pose2 = strstr(pose2, argv[2]);
+        pose2 += strlen(argv[2]);
+        strcpy(pose,pose2);
+    }
+    printf("%s", string);
     return 0;
 }
