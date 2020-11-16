7802cd9dda27384ccd0a49eac2e99a64f87f1b99
R H <m.rehamed@gmail.com>
2020-11-16 21:30:35 +0100

    initialisation de l'exercice

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include "func.h"
 //to compile gcc main.c func.c -o prg.exe don't need .exe if you are on mac
+//["chemise fantaisie", "pantalon velours", "chemise fantaisie", "tongues", "bluejeans", "bluejeans"]
 int main(int argc, char ** argv) {
     return 0;
 }
